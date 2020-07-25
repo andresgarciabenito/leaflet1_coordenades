c3f653900dfc58be961bb015b51ba57293d5117c
andresgarciabenito <andresgarciabenito@hotmail.com>
2020-07-25 13:07:22 +0200

    ex1.fase2

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-//EX1. FASE1. Crear un mapa utilitzant leaflet i que tingui com a centre La Plaça Catalunya
-var mymap = L.map('mapid').setView([41.386963, 2.170045], 17);
+//EX1. FASE1. Crear un mapa utilitzant leaflet i que tingui com a centre La Plaça Catalunya.
+var mymap = L.map('mapid').setView([41.387225,2.168548], 17);
 
 L.tileLayer('https://api.maptiler.com/maps/bright/{z}/{x}/{y}.png?key=kVYnuZ3P22KkA5yZKlai', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
@@ -10,3 +10,5 @@ L.tileLayer('https://api.maptiler.com/maps/bright/{z}/{x}/{y}.png?key=kVYnuZ3P22
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
+//EX1. FASE2. Posar un marcador a la ubicació Carrer Balmes 16.
+var marker = L.marker([41.386943, 2.165957]).addTo(mymap);
